0a138393fde480344a27f9a536da168997a5e3eb
webdevyoo <yjk7454@naver.com>
2024-03-03 13:48:51 +0900

    feat: app - controller,service제거 및 module import 초기화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+/**
+ * NestJS Decorator 함수
+ * 클래스에 함수 기능을 추가할 수 있다.
+ * 클래스 위의 함수로써 클래스를 위해 작동된다.
+ * 현재 클래스인 AppModule는 비어있으나 실질적인 코드는 @Module에 있다.
+ */
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
