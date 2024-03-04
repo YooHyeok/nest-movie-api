6625bb9a4eca3650c56b54666eaaebd5953d0a44
webdevyoo <yjk7454@naver.com>
2024-03-04 17:46:44 +0900

    feat: movies.service 각 crud 실체 구현 및 update 추가

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,17 +10,32 @@ export class MoviesService {
   }
   getOne(id: string): Movie {
     // return this.movies.find((movie) => movie.id === parseInt(id));
-    return this.movies.find((movie) => movie.id === +id); // + 기호를 통해 id를 number로 타입변경
+    const movie = this.movies.find((movie) => movie.id === +id); // + 기호를 통해 id를 number로 타입변경
+    console.log(movie)
+    if (!movie) {
+      throw new NotFoundException(`Movie With ID: ${id} not found.`); // Nest JS에서 지원하는 HttpException의 확장된 Excption
+    }
+    return movie;
   }
   deleteOne(id: string): boolean {
-    this.movies.filter((movie) => movie.id !== +id);
+    this.getOne(id); // 조회되는지 여부 파악 안되면 NotFoundException에러 발생됨
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
     return true;
   }
-  create(movieData: object) {
-    console.log(movieData);
+  create(movieData) { // 성공시 201 코드 반환
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData,
-    })
+    });
+  }
+  update(id: string, updateData) {
+    console.log(id);
+    console.log(updateData);
+    const movie = this.getOne(id); // 조회되는지 여부 파악 안되면 NotFoundException에러 발생됨
+    this.deleteOne(id);
+    this.movies.push({
+      ...movie, // 기존 데이터 전개
+      ...updateData, // 신규 데이터로 변경
+    });
   }
 }
