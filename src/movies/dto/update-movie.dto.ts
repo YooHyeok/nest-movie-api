009684f8488e359dad5a3fd5169b1e5860880786
webdevyoo <yjk7454@naver.com>
2024-03-05 00:46:11 +0900

    fix: update-move.dto PartialType 적용 수정.... (주석잘못적용됨)
    주석이 잘못 되어있었다...

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,14 +1,13 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDTO } from './create-movie.dto';
 
-export class UpdateMovieDTO {
+/* export class UpdateMovieDTO {
   @IsString()
   readonly title?: string;
   @IsNumber()
   readonly year?: number;
   @IsString({ each: true })
   readonly genres?: string[];
-}
+} */
 
-// export class UpdateMovieDTO extends PartialType(CreateMovieDTO) {}
+export class UpdateMovieDTO extends PartialType(CreateMovieDTO) {}
