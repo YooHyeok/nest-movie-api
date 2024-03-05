be85cbfaa70d9a2ee5ff6853ba497e0f61a5a768
webdevyoo <yjk7454@naver.com>
2024-03-05 11:51:36 +0900

    feat: spec.ts 테스트 파일 임시 단위 테스트 코드 추가

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { describe, it } from 'node:test';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  /**
+   * 실제 테스트 실행전 실행된다.
+   */
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +16,14 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  /**
+   * individual test의 줄임말
+   */
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be 4', () => {
+    expect(2 + 2).toEqual(5);
   });
 });
