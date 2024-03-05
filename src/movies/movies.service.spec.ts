fd09e0bb515b72b5132718fdac6a019b42a611b7
webdevyoo <yjk7454@naver.com>
2024-03-06 01:11:58 +0900

    test: movies.service.spec JEST update, 404 단위 테스트

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -86,4 +86,25 @@ describe('MoviesService', () => {
       expect(afterCreate).toBeGreaterThan(beforeCreate); // 추가 후가 추가 전보다 크다.
     });
   });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Update Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Update Test');
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(1, { title: 'Update Test' }); // Movies가 비어있음..
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
